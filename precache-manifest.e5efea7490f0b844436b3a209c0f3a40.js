b11a8f8442e7cdf451658d683dc0951e43c45f74
elisanauha <elisa.nauha@gmail.com>
2019-03-24 20:19:53 +0200

    Updates

---
+++ precache-manifest.e5efea7490f0b844436b3a209c0f3a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61ef8762a3443f9e08ba",
+    "url": "/vr-api/static/js/runtime~main.235a56e1.js"
+  },
+  {
+    "revision": "071c49a16aa19c076c27",
+    "url": "/vr-api/static/js/main.1e8e15ff.chunk.js"
+  },
+  {
+    "revision": "3e9695650bc64bd28618",
+    "url": "/vr-api/static/js/2.4548674f.chunk.js"
+  },
+  {
+    "revision": "071c49a16aa19c076c27",
+    "url": "/vr-api/static/css/main.d2e40e14.chunk.css"
+  },
+  {
+    "revision": "ce3ec62a2906bf268104d0a58e6253b7",
+    "url": "/vr-api/index.html"
+  }
+];
